4cbcc62b5fb22e55032ef21fb448636d3df18e2e
Felipe Ulloa <felipe.ulload@alumnos.uv.cl>
2016-04-13 16:57:06 -0300

    tabla dinamica

--- Clase2/js/Jquery.js
+++ Clase2/js/Jquery.js
@@ -1,4 +1,16 @@
 var cont = 0;
+var miJSON={
+	"A1":{"user":"usuario 1", "name":"name 1", "lastName":"apellido 1"},
+	"A2":{"user":"usuario 2", "name":"name 2", "lastName":"apellido 2"},
+	"A3":{"user":"usuario 3", "name":"name 3", "lastName":"apellido 3"},
+}
+
+$(document).ready(function(){
+	$.each(miJSON, function(i,item){
+		var fila='<tr id="'+cont+'" onclick="mostrar_seleccionados(this.id)"><td>'+miJSON[i].user+'</td><td>'+miJSON[i].name+'</td><td>'+miJSON[i].lastName+'</td>'+'</tr>';
+		$('#datos_tabla').append(fila);
+	})
+})
 //Agregar una fila con los datos de los inputs 
 $('#btn_agregar').click(function() {
 	if ($('#user').val() || $('#nombre').val() || $('#apellido').val()) {
@@ -9,7 +21,7 @@ $('#btn_agregar').click(function() {
 		//creo una nueva fila
 		var fila='<tr id="'+cont+'" onclick="mostrar_seleccionados(this.id)"><td>'+user+'</td><td>'+nombre+'</td><td>'+apellido+'</td>'+'</tr>';
 		//añado fila a la tabla
-		$('tbody').append(fila);
+		$('#datos_tabla').append(fila);
 		$('#user').val("");
 		$('#nombre').val("");
 		$('#apellido').val("");

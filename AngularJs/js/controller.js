f62df7d2c329f5b4fb6b158a8fc010cdedfcd013
Felipe Ulloa <felipe.ulload@alumnos.uv.cl>
2016-04-13 23:43:25 -0300

    Agregar funcionalidad del boton agregar

--- AngularJs/js/controller.js
+++ AngularJs/js/controller.js
@@ -3,22 +3,30 @@ app.controller("testController", function($scope) {
 	$scope.newUser = {
 		user:"",
 		name:"",
-		lastname:""
+		lastName:""
 	};
 
 	$scope.users = [
 		{user: "usuario 1",
 		name: "name 1",
-		lastname: "apellido 1"},
+		lastName: "apellido 1"},
 
 		{user: "usuario 2",
 		name: "name 2",
-		lastname: "apellido 2"},
+		lastName: "apellido 2"},
 
 		{user: "usuario 3",
 		name: "name 3",
-		lastname: "apellido 3"}
+		lastName: "apellido 3"}
 	];
+	
+	$scope.addNewUser = function (user) {
+		 console.log(user);
+		 $scope.users.push(angular.extend({},user));
+		 // console.log(angular.copy({},user));
+		 // console.log(angular.extend({},user));
+		 // console.log(angular.merge({},user));
+	}
 
 	$scope.showDetails= function (user) {
 		console.log(user);
